e3d334ad1a8e63d6fe43c9c37f1570c2adfd0d3b
rmapr <rmap@ukr.net>
2023-06-21 23:07:20 +0300

    Minor bug fixes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,22 +1,20 @@
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class Main {
 
     public static void main(String[] args) {
         List<Integer> integerList = RandomList.getIntegerArrayList(10);
-        Service serv = new Service();
 
         System.out.println(integerList);
-        System.out.println(serv.averageValue(integerList));
-        System.out.println(serv.averageValue(Arrays.asList(1, 2, 3, 4, 5)));
+        System.out.println(Service.averageValue(integerList));
+        System.out.println(Service.averageValue(Arrays.asList(1, 2, 3, 4, 5)));
 
         List<String> stringList = Arrays.asList("one", "two", "three");
-        System.out.println(serv.getPairList(stringList));
+        System.out.println(Service.getPairList(stringList));
 
         stringList = Arrays.asList("Clock", "Banana", "Time", "date", "lamp");
-        System.out.println(serv.printLowerCaseLengthFour(stringList));
+        System.out.println(Service.printLowerCaseLengthFour(stringList));
     }
 
 }
